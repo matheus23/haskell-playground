1d0134bdbedba4f086176573352500db1cb721f5
Philipp Krüger <matheus.dev@gmail.com>
2017-11-30 12:41:53 +0100

    Remove NbE module from default exports.

--- src/Typed.hs
+++ src/Typed.hs
@@ -4,7 +4,6 @@ module Typed
   , module Typed.PrettyPrint
   , module Typed.TypeCheck
   , module Typed.Interpreter
-  , module Typed.NormalizationByEvaluation
   ) where
 
 import Typed.Lambda
@@ -12,4 +11,3 @@ import Typed.Parser
 import Typed.PrettyPrint
 import Typed.TypeCheck
 import Typed.Interpreter
-import Typed.NormalizationByEvaluation
